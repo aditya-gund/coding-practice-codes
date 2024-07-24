f2d894e2432bfba342c141cbffb077ef3b9a7144
Aditya <adityagundg14@gmail.com>
2024-07-24 23:11:06 +0530

    Optimised O(n)

--- Day1/1.Two Sum/Brute force/twoSum.java
+++ Day1/1.Two Sum/Brute force/twoSum.java
@@ -1,27 +1,32 @@
-class twoSum{
+import java.util.HashMap;
+import java.util.Map;
+
+class twoSum {
     public static int[] twoSumFun(int[] nums, int target) {
-        // Length of the nums array
-        int len=nums.length;
-        //intializing the result array to store the results
-        int[] result = new int[2];
-        // Outer Loop start from 0th index and runs upto the last index value of the arary
-        for(int i=0;i<=len-1;i++){
-            // Inner loop start from the 1st index run until the last index of the array
-            for(int j=i+1;j<=len-1;j++){
-                // Check if prev index value and curr value is == target value
-                if(nums[i]+nums[j]==target){
-                    result[0]=i;
-                    result[1]=j;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            
+            // Check if the difference exists in the map
+            if (map.containsKey(diff)) {
+                ans[0] = map.get(diff);  // index of the previously stored number
+                ans[1] = i;              // current index
+                break;  // Found the solution, so break out of the loop
             }
+            
+            // Store the current number and its index in the map
+            map.put(nums[i], i);
         }
         
-        return result;
+        return ans;
     }
+    
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] indices = twoSumFun(nums, target);
-        System.out.println("Indices: [" + indices[0] + ", " + indices[1]+"]");
+        System.out.println("Indices: [" + indices[0] + ", " + indices[1] + "]");
     }    
 }
